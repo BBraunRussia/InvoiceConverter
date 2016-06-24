bf2bab564fbc11ddf80e59c81e5350f4d35db2df
Pavel Maslyaev <pavel-max@rambler.ru>
2016-06-24 16:09:20 +0300

    добавил класс GrandCapital

---
+++ InvoiceConvert/Companies/GrandCapital.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using InvoiceConverter;
+
+namespace InvoiceConverter.Companies
+{
+    public class GrandCapital : ConvFile
+    {
+        protected override void CreateFileName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CreateAndSaveFile()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
