966599bcc2d19d9d314cc7e836495865c12f4042
Pavel Maslyaev <pavel-max@rambler.ru>
2016-06-24 15:58:59 +0300

    добавил нового покупателя

--- InvoiceConvert/enums.cs
+++ InvoiceConvert/enums.cs
@@ -3,6 +3,6 @@
     public enum Cust
     {
         Katren = 1, Shaklin = 2, AptekaSkal = 3, Protek = 4, EuropeanMedicalCenter = 5, Optimed = 6,
-        Voltars = 7, Optimed2 = 8, UralApteka = 9, AnteyFarma = 10, GBUZ = 11, SeveroZapad = 12
+        Voltars = 7, Optimed2 = 8, UralApteka = 9, AnteyFarma = 10, GBUZ = 11, SeveroZapad = 12, GrandCapital = 13
     };
 }
