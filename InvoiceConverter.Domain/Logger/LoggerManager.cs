e4d9100abc96291ff3cfe462f70ef50ddcd83ba5
Pavel Maslyaev <pavel-max@rambler.ru>
2016-10-05 11:26:04 +0300

    рефакторинг

--- InvoiceConverter.Domain/Logger/LoggerManager.cs
+++ InvoiceConverter.Domain/Logger/LoggerManager.cs
@@ -18,29 +18,5 @@ namespace InvoiceConverter.Domain.Logger
             .CreateLogger();
 
         public static ILogger Logger { get { return logger; } }
-
-        public static void FileProcessed(string file, string fileNew)
-        {
-            Write(string.Concat("Файл ", file, " был конвертирован в файл ", fileNew));
-        }
-
-        public static void ErrorCreated(string fileName, string error)
-        {
-            Write(string.Concat("Ошибка при обработке файла ", fileName, " - ", error));
-            Sender.SendMail(Settings.adminEmail, "Ошибка при обработке файла", "Ошибка при обработке файла, описание ошибки в лог файле");
-        }
-
-        public static void FolderCreated(string folder)
-        {
-            Write("Создание папки " + folder);
-        }
-        
-        private static void Write(string Message)
-        {
-            using (StreamWriter swLog = new StreamWriter("log.txt", true, Encoding.Unicode))
-            {
-                swLog.WriteLine(string.Concat(DateTime.Now.ToString(), ": ", Message));
-            }
-        }
     }
 }
